247a037575bbb7dbad3ef522ae2ad632e7737479
Sam Galizia <samgalizia@yahoo.com>
2016-06-26 20:48:23 -0400

    add: Outcome enumeration

---
+++ RockPaperScissors/Outcome.swift
@@ -0,0 +1,15 @@
+//
+//  Outcome.swift
+//  RockPaperScissors
+//
+//  Created by Sam on 6/26/16.
+//  Copyright © 2016 Sam Galizia. All rights reserved.
+//
+
+import Foundation
+
+enum Outcome {
+  case Win
+  case Lose
+  case Tie
+}
